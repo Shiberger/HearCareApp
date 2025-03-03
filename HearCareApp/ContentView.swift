7466ea12bc2b1b6adfbe0822ad8c279856761985
Shiberger <hannarong.tonkra@gmail.com>
2025-03-03 09:19:27 +0700

    Initial Commit

---
+++ HearCareApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HearCareApp
+//
+//  Created by Hannarong Kaewkiriya on 3/3/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HearCareApp/HearCareAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HearCareAppApp.swift
+//  HearCareApp
+//
+//  Created by Hannarong Kaewkiriya on 3/3/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct HearCareAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
